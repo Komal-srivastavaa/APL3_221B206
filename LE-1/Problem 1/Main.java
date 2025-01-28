b75b6e53d7cf18f296a03a47adc5a09fbb897a02
221B206 <221B206@juet.co.in>
2025-01-28 11:48:33 +0530

    initial commit

---
+++ LE-1/Problem 1/Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		Mother m= new Mother ( );
+		m.x=11;
+        m.show( ); // show of Mother is called
+        Child ch=new Child ( ); 
+        ch.x=12;
+        ch. show ( ); // show ( )
+	}
+}
